9a0211067f86e6dded029053fe7fcb1c2e65055e
william <hpl.jack@gmail.com>
2023-05-11 23:10:39 +0800

    [mod] use coercions number to string

--- Part1-Basics/C03-Numbers/tolerance.lua
+++ Part1-Basics/C03-Numbers/tolerance.lua
@@ -1,5 +1,8 @@
+
 local tolerance = 10
 function isturnback (angle)
-angle = angle % 360
-return (math.abs(angle - 180) < tolerance)
+    angle = angle % 360
+    return (math.abs(angle - 180) < tolerance)
 end
+
+print(isturnback(-180))
