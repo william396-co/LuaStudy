2e3227d76d40601e4cc7287185512110318e1241
huiping.liu <huiping.liu@silverkeyz.com>
2023-05-17 19:22:19 +0800

    metatable code sample

---
+++ ProgrammingInLua/C20-MetatablesAndMetamethods/metatables_example.lua
@@ -0,0 +1,17 @@
+t ={}
+print(getmetatable(t)) -- nil
+
+t1 = {}
+setmetatable(t,t1)
+print(getmetatable(t) == t1) -- true
+
+print("===========")
+print(getmetatable(t)) -- table: 0x1679ac0
+print(getmetatable(t1))-- nil
+
+
+print("===========")
+print(getmetatable("hi")) -- table: 0x1676e50
+print(getmetatable("xix"))-- table: 0x1676e50
+print(getmetatable(10)) -- nil
+print(getmetatable(print)) -- nil
