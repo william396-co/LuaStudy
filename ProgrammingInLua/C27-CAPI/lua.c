70443efa9e25cfdac6984075eb54380657b999c0
huiping.liu <huiping.liu@silverkeyz.com>
2023-06-07 15:38:35 +0800

    modified lua.c

--- ProgrammingInLua/C27-CAPI/lua.c
+++ ProgrammingInLua/C27-CAPI/lua.c
@@ -3,7 +3,20 @@
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
+#include <stdarg.h>
+#include <stdlib.h>
 
+/*
+void error( lua_State * L, const char * fmt, ... )
+{
+    va_list argp;
+    va_start( argp, fmt );
+    vfprintf( stderr, fmt, argp );
+    va_end( argp );
+    lua_close( L );
+    exit( EXIT_FAILURE );
+}
+*/
 int main()
 {
     char buff[256];
